744e902323c6100339cc0f85da92ee860650ed17
Raphael Andrews <raphaelandrews95@gmail.com>
2023-03-03 17:37:44 -0300

    refactor: modal style on header

---
+++ src/components/Modal/Modal.styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ModalOverlay = styled.div`
+    position:fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.7);
+    z-index: 9999;
+    @media (min-width: 800px) {
+        display: none;
+    }
+`;
+
+export const ModalBox = styled.div`
+    position: fixed;
+    top: 6rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 240px;
+    max-width: 90%;
+    min-height: 240px;
+    max-height: 90%;
+    padding: 3rem 0;
+    border-radius: .75rem;
+    background: var(--bg-alt-color);
+`;
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import styled from 'styled-components';
+import * as C from "./Modal.styles"
 
 interface ModalType {
     children?: ReactNode;
@@ -7,40 +7,15 @@ interface ModalType {
     toggle: () => void;
 }
 
-const ModalOverlay = styled.div`
-    position:fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.7);
-    z-index: 9999;
-    @media (min-width: 800px) {
-        display: none;
-    }
-`;
-
-const ModalBox = styled.div`
-    display: block;
-    width: 70%;
-    height: 70%;
-    padding: 1rem;
-    border-radius: 1rem;
-    background: white;
-`;
-
 export default function Modal(props: ModalType) {
     return (
         <>
             {props.isOpen && (
-                <ModalOverlay onClick={props.toggle}>
-                    <ModalBox onClick={(e) => e.stopPropagation()}>
+                <C.ModalOverlay onClick={props.toggle}>
+                    <C.ModalBox onClick={(e) => e.stopPropagation()}>
                         {props.children}
-                    </ModalBox>
-                </ModalOverlay>
+                    </C.ModalBox>
+                </C.ModalOverlay>
             )}
         </>
     );
